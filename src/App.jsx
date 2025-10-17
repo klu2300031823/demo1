3418716b48fca5318dc4855594dca235ea150f39
M.V.B.N SREEGANESH <163726114+klu2300031823@users.noreply.github.com>
2025-10-17 21:16:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/CICD_Skill-3">
+    <Router basename="/demo1">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
